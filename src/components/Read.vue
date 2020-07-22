f75125b646c4e6e60482e17a5efc56cee74b7830
Shehab Shalan <shehab.shalan@hotmail.com>
2020-07-23 03:56:18 +0800

    pdf-cors-fixed

--- src/components/Read.vue
+++ src/components/Read.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <Navbar />
-    <v-container fluid class="mx-auto">
+    <v-container fluid>
       <h1>تقرأ الان {{novel.title}}</h1>
       <div class="novel-display">
         <!-- <pdf src="https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK" :page="1"></pdf> -->
-        <pdf :src="novel.novel_url" :page="1"></pdf>
+        <pdf :src="novel.novel_url" :page="10"></pdf>
       </div>
-      <p>{{novel.novel_url}}</p>
     </v-container>
     <Footer />
   </div>
@@ -36,11 +35,12 @@ export default {
       .where("novel_slug", "==", this.$route.params.novel_slug);
     ref.get().then(snapshot => {
       snapshot.forEach(doc => {
-        console.log(doc.data());
+        // console.log(doc.data());
         this.novel = doc.data();
         this.novel.id = doc.id;
       });
     });
+    // this.$vuetify.rtl = false;
   }
 };
 </script>
@@ -48,5 +48,7 @@ export default {
 <style scoped>
 .novel-display {
   border-style: solid;
+  width: 50%;
+  height: 10%;
 }
 </style>
